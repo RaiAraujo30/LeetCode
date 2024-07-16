65e086b922059b32d88f0867f6256e483c8785e0
Rai Araujo <raisanhacu@gmail.com>
2024-07-16 19:07:47 -0300

    Palindromo resolvido

--- palindrome.js
+++ palindrome.js
@@ -2,14 +2,17 @@
  * @param {number} x
  * @return {boolean}
  */
+
 var isPalindrome = function(x) {
 
     var str = x.toString()
     var newArray = str.split('').reverse().join('')
+    
+    console.log(newArray)
 
    return newArray === str
 
     
 };
 
-console.log(isPalindrome(121))
+console.log(isPalindrome(123))
